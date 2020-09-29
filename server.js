96a428fca3bd83dae6266512a7eff6dcafd6ff31
Weerklank <Zoe.veggie@gmail.com>
2020-09-28 20:49:20 -0700

    added process.env

--- server.js
+++ server.js
@@ -1,26 +1,6 @@
-// var express = require("express");
-// var handlebars = require("express-handlebars");
-
-// var PORT = process.env.PORT || 8000;
-// var app = express();
-
-// app.use(express.static("public"));
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// app.engine("handlebars", handlebars({ defaultLayout: "main" }));
-// app.set("view engine", "handlebars");
-
-// app.use(routes);
-
-// app.listen(PORT, function() {
-//   console.log("Listening on port:%s", PORT);
-// });
-
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const routes = require("./controllers/burger_controller.js");
 
 const handlebars = require('express-handlebars');
