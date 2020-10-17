7c6d476c1605f41385c24d4602cccc9e7fda010f
Weerklank <Zoe.veggie@gmail.com>
2020-10-17 15:38:33 -0700

    fixing heroku crash timeout

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,5 @@
 var mysql = require("mysql");
 
-// var connection = mysql.createConnection({
-//   host: process.env.MYSQL_HOST || "us-cdbr-east-02.cleardb.com",
-//   port: process.env.PORT || 3000,
-//   user: process.env.MYSQL_USERNAME || "b9e079a0cf7bc4",
-//   password: process.env.MYSQL_PASSWORD || "b4317ad9",
-//   database: process.env.MYSQL_DATABASE || "heroku_d89b69fad187ac2"
-// });
-
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
