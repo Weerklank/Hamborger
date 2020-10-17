6bb18ac744b1c00feeae8f3af094ca6ba6b9de9a
Weerklank <Zoe.veggie@gmail.com>
2020-10-17 14:44:56 -0700

    Making sql work with heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,24 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  port: process.env.PORT,
-  user: process.env.MYSQL_USERNAME,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE
-});
+// var connection = mysql.createConnection({
+//   host: process.env.MYSQL_HOST || "us-cdbr-east-02.cleardb.com",
+//   port: process.env.PORT || 3000,
+//   user: process.env.MYSQL_USERNAME || "b9e079a0cf7bc4",
+//   password: process.env.MYSQL_PASSWORD || "b4317ad9",
+//   database: process.env.MYSQL_DATABASE || "heroku_d89b69fad187ac2"
+// });
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST || "localhost",
+    port: process.env.PORT || 3000,
+    user: process.env.MYSQL_USERNAME || "root",
+    password: process.env.MYSQL_PASSWORD || "",
+    database: process.env.MYSQL_DATABASE || "hamburger_db"
+  })
+}
 
 connection.connect(function(err) {
   if (err) {
