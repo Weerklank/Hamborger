36b403717745ebfbb7bc22c2acf1a83d70f70978
Weerklank <Zoe.veggie@gmail.com>
2020-10-16 20:44:03 -0700

    Changing to make MySQL work with Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 var mysql = require("mysql");
 
-var connection = process.env.createConnection || mysql.createConnection({
+var connection = mysql.createConnection({
   host: process.env.MYSQL_URL || "localhost",
   port: process.env.PORT || 3306,
-  user: process.env.MYSQL_USERNAME || "root",
+  user: "root",
   password: process.env.MYSQL_PASSWORD || "",
-  database: process.env.MYSQL_DATABASE || "burgers_db"
+  database: "burgers_db"
 });
 
 connection.connect(function(err) {
