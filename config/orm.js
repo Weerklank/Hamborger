a9732674783173c2ef3ee55810374070c2bc9046
Weerklank <Zoe.veggie@gmail.com>
2020-10-16 21:20:33 -0700

    Changing to make MySQL work with Heroku

--- config/orm.js
+++ config/orm.js
@@ -3,14 +3,12 @@ var connection = require("./connection.js")
 var orm = {
     selectAll: function (fn) {
         connection.query("SELECT * FROM burgers", function (err, result) {
-            if (err) throw err;
             fn(result)
         });
     },
     insertOne: function (name, devoured, fn) {
         const query = `INSERT INTO burgers (burger_name, devoured) VALUES ("${name}",${devoured})`
         connection.query(query, function (err, result) {
-            if (err) throw err;
             fn(result)
         });
     },
@@ -18,7 +16,7 @@ var orm = {
         connection.query(`SELECT * FROM burgers WHERE id=${id}`, function (err, result) {
             const query = `UPDATE burgers SET burger_name = "${result[0].burger_name}", devoured = ${devoured} WHERE id = ${id}`
             connection.query(query, function (err, result) {
-                if (err) throw err;
+
                 fn(result)
             });
         });
